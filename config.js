e02b3015f9e4ea919f21eac8a63ec29ba6282307
Martin Giger <martin@humanoids.be>
2018-11-07 18:18:52 +0100

    Add suggestions that are completely untested

--- config.js
+++ config.js
@@ -2,8 +2,9 @@
     "use strict";
     var twitch = window.Twitch.ext;
     var iframe = document.getElementById("embed");
-    var inputs = document.querySelectorAll("input");
+    var inputs = document.querySelectorAll("input[name]");
     var videoId = document.querySelector('[name="videoId"]');
+    var results = document.getElementById("vodResults");
     function getConfig() {
         var config = {};
         for(var i of inputs) {
@@ -13,6 +14,64 @@
         }
         return config;
     }
+    function clearResults() {
+        while(results.firstElementChild) {
+            results.firstElementChild.remove();
+        }
+    }
+    function getType() {
+        return document.querySelector('input[name="type"]:checked').value;
+    }
+    function addSuggestion(id, title, thumbnail, viewCount, duration) {
+        var root = document.createElement("li");
+        var title = document.createElement("h3");
+        title.textContent = title;
+        var thumb = new Image();
+        thumb.src = thumbnail;
+        root.appendChild(title);
+        root.appendChild(thumb);
+        root.title = `Use ${id}`;
+
+        //TODO add view count and optionally duration
+
+        root.addEventListener("click", (e) => {
+            document.querySelector('[name="videoId"]').value = id;
+        }, false);
+
+        results.appendChild(root);
+    }
+    function searchVideos() {
+        clearResults();
+        if(!this.channelId || this.clientId) {
+            return;
+        }
+        if(getType() === "clip") {
+            fetch(`https://api.twitch.tv/helix/clips?broadcaster_id=${this.channelId}`, {
+                headers: {
+                    'Client-ID': this.clientId
+                }
+            }).then((res) => {
+                return res.json();
+            }).then((result) => {
+                for(const clip of result.data) {
+                    addSuggestion(clip.id, clip.title, clip.thumbnail_url, clip.view_count);
+                }
+            })
+        }
+        else { // VOD
+            fetch(`https://api.twitch.tv/helix/videos?user_id=${this.channelId}`, {
+                headers: {
+                    'Client-ID': this.clientId
+                }
+            }).then((res) => {
+                return res.json();
+            }).then((result) => {
+                for(const vod of result.data) {
+                    addSuggestion(`v${vod.id}`, vod.title, vod.thumbnail_url, vod.view_count, vod.duration);
+                }
+            });
+        }
+    }
     document.querySelector("form").addEventListener("submit", function(e) {
         e.preventDefault();
         twitch.configuration.set('broadcaster', '1.0.0', JSON.stringify(getConifg()));
@@ -20,6 +79,7 @@
     document.addEventListener("input", function(e) {
         showVideo(iframe, getConfig());
         if(e.target.name === "type" && e.target.checked) {
+            searchVideos();
             if(e.target.value === "video") {
                 videoId.pattern = 'v[0-9]+';
                 videoId.placeholder = 'v123456789';
@@ -30,7 +90,9 @@
             }
         }
     }, true);
-    twitch.onAuthorized(function() {
+    twitch.onAuthorized(function(info) {
+        searchVideos.channelId = info.channelId;
+        searchVideos.clientId = info.clientId;
     });
     twitch.configuration.onChanged(function() {
         if(twitch.configuration.broadcaster) {
@@ -38,10 +100,10 @@
             for(var k in config) {
                 if(config.hasOwnProperty(k)) {
                     if(k == "type") {
-                        document.querySelector('[name="type"][value="'+config[k]+'"]').checked = true;
+                        document.querySelector('input[name="type"][value="'+config[k]+'"]').checked = true;
                     }
                     else {
-                        document.querySelector('[name="'+k+'"]').value = config[k];
+                        document.querySelector('input[name="'+k+'"]').value = config[k];
                     }
                 }
             }
