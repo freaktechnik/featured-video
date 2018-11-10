e66f3e69ba28ceb1922ecd13f0db412b4879968a
Martin Giger <martin@humanoids.be>
2018-11-11 00:56:35 +0100

    feat: various fixes to the video selector

--- config.js
+++ config.js
@@ -5,6 +5,7 @@
     var inputs = document.querySelectorAll("input[name]");
     var videoId = document.querySelector('[name="videoId"]');
     var results = document.getElementById("vodResults");
+    var recommendationCount = 5;
     function getConfig() {
         var config = {};
         for(var i of inputs) {
@@ -22,13 +23,32 @@
     function getType() {
         return document.querySelector('input[name="type"]:checked').value;
     }
+    function updateVideo() {
+        var config = getConfig();
+        document.getElementById("noEmbed").className = config.videoId ? 'hidden' : '';
+        iframe.className = config.videoId ? '' : 'hidden';
+        showVideo(iframe, config);
+    }
     function addSuggestion(id, title, thumbnail, viewCount, duration) {
         var root = document.createElement("li");
-        var title = document.createElement("h3");
-        title.textContent = title;
+        var titleEl = document.createElement("h3");
+        titleEl.textContent = title;
         var thumb = new Image();
-        thumb.src = thumbnail;
-        root.appendChild(title);
+        thumb.src = thumbnail.replace('%{height}', 80).replace('%{width}', 142);
+        var description = document.createElement("p");
+        var viewCountEl = document.createElement("span");
+        viewCountEl.textContent = `Views: ${viewCount}`;
+        var contentWrapper = document.createElement("div");
+        contentWrapper.className = 'wrapper';
+        description.appendChild(viewCountEl);
+        if(duration) {
+            var durationEl = document.createElement("span");
+            durationEl.textContent = `Duration: ${duration}`;
+            description.appendChild(durationEl);
+        }
+        contentWrapper.appendChild(titleEl);
+        contentWrapper.appendChild(description);
+        root.appendChild(contentWrapper);
         root.appendChild(thumb);
         root.title = `Use ${id}`;
 
@@ -36,19 +56,20 @@
 
         root.addEventListener("click", (e) => {
             document.querySelector('[name="videoId"]').value = id;
+            updateVideo();
         }, false);
 
         results.appendChild(root);
     }
     function searchVideos() {
         clearResults();
-        if(!this.channelId || this.clientId) {
+        if(!searchVideos.channelId || !searchVideos.clientId) {
             return;
         }
         if(getType() === "clip") {
-            fetch(`https://api.twitch.tv/helix/clips?broadcaster_id=${this.channelId}`, {
+            fetch(`https://api.twitch.tv/helix/clips?broadcaster_id=${searchVideos.channelId}&first=${recommendationCount}`, {
                 headers: {
-                    'Client-ID': this.clientId
+                    'Client-ID': searchVideos.clientId
                 }
             }).then((res) => {
                 return res.json();
@@ -59,9 +80,9 @@
             })
         }
         else { // VOD
-            fetch(`https://api.twitch.tv/helix/videos?user_id=${this.channelId}`, {
+            fetch(`https://api.twitch.tv/helix/videos?user_id=${searchVideos.channelId}&first=${recommendationCount}`, {
                 headers: {
-                    'Client-ID': this.clientId
+                    'Client-ID': searchVideos.clientId
                 }
             }).then((res) => {
                 return res.json();
@@ -77,7 +98,7 @@
         twitch.configuration.set('broadcaster', '1.0.0', JSON.stringify(getConifg()));
     });
     document.addEventListener("input", function(e) {
-        showVideo(iframe, getConfig());
+        updateVideo();
         if(e.target.name === "type" && e.target.checked) {
             searchVideos();
             if(e.target.value === "video") {
@@ -93,6 +114,7 @@
     twitch.onAuthorized(function(info) {
         searchVideos.channelId = info.channelId;
         searchVideos.clientId = info.clientId;
+        searchVideos();
     });
     twitch.configuration.onChanged(function() {
         if(twitch.configuration.broadcaster) {
@@ -107,7 +129,7 @@
                     }
                 }
             }
-            showVideo(iframe, getConfig());
+            updateVideo();
         }
     });
     twitch.onContext(function(context) {
